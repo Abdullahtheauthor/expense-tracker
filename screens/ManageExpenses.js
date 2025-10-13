fda1e0817e95948609ca1f73053ecd6dd2578363
Abdullahtheauthor <abdullahgoda197@gmail.com>
2025-10-13 21:55:56 +0300

    setting mangeexpense screenoptions using useLayoutEffect depending on isEditing

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,7 +1,14 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
-function ManageExpenses({ route }) {
-  const editedExpenseId = route.params.expenseId;
+function ManageExpenses({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expenses" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text>Manage Expenses Screen {editedExpenseId}</Text>;
 }
 
