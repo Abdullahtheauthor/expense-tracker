a389353d0252f6edbe2527b0f8cfce096eed039f
Abdullahtheauthor <abdullahgoda197@gmail.com>
2025-10-13 21:48:43 +0300

    using git add . to convert editedExpenseId to boolean. If it is falsy value like undefined, this means it is false and vice versa

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,6 +1,7 @@
 import { Text } from "react-native";
 function ManageExpenses({ route }) {
-  const editedExpenseId = route.params?.expenseId;
+  const editedExpenseId = route.params.expenseId;
+  const isEditing = !!editedExpenseId;
   return <Text>Manage Expenses Screen {editedExpenseId}</Text>;
 }
 
