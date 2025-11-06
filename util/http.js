176f68c0dd5e64821ca34de2776c981860f7ceaf
Abdullahtheauthor <abdullahgoda197@gmail.com>
2025-11-06 20:17:24 +0200

    fixing fetchiing data

--- util/http.js
+++ util/http.js
@@ -7,7 +7,19 @@ export function storeExpense(expenseData) {
   axios.post(BASIC_URL + "/expense.json", expenseData);
 }
 
-export async function getExpense(expenseData) {
-  const response = awaitaxios.post(BASIC_URL + "/expense.json");
-  console.log(response);
+export async function fetchExpense(expenseData) {
+  const response = await axios.get(BASIC_URL + "/expense.json");
+  console.log(response.data);
+
+  const expenses = [];
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+  return expenses;
 }
